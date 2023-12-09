f5c446b4a78a280428349134e16bf0107d77436f
BernardinoSlv <bernardinoosilvaa@gmail.com>
2023-12-09 10:15:46 -0300

    refactor view entry.index: table size

--- resources/views/entry/index.blade.php
+++ resources/views/entry/index.blade.php
@@ -5,28 +5,30 @@
         <a href="{{ route('entry.create') }}" class="btn btn-primary">Criar nova</a>
     </div>
 
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Título</th>
-                <th>Valor</th>
-                <th>Tipo</th>
-                <th>Data</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($entries as $entry)
+    <div class="table-responsive">
+        <table class="table table-hover" style="min-width: 700px">
+            <thead>
                 <tr>
-                    <td>{{ $entry->id }}</td>
-                    <td>{{ $entry->title }}</td>
-                    <td>R$ {{ $entry->amount }}</td>
-                    <td><span class="badge text-white bg-primary">Pagamento</span></td>
-                    <td>{{ $entry->created_at }}</td>
-                    <td></td>
+                    <th>#</th>
+                    <th>Título</th>
+                    <th>Valor</th>
+                    <th>Tipo</th>
+                    <th>Data</th>
+                    <th>Ações</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($entries as $entry)
+                    <tr>
+                        <td>{{ $entry->id }}</td>
+                        <td>{{ $entry->title }}</td>
+                        <td>R$ {{ $entry->amount }}</td>
+                        <td><span class="badge text-white bg-black">Padrão</span></td>
+                        <td>{{ $entry->created_at }}</td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
