93dafb653e5924ac5eccf12853607c9560d75232
BernardinoSlv <bernardinoosilvaa@gmail.com>
2023-12-09 09:29:15 -0300

    feat Entry model: set amount attribute to parse to float

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Src\Parsers\ToFloatParser;
 
 class Entry extends Model
 {
@@ -14,4 +16,13 @@ class Entry extends Model
         "description",
         "amount",
     ];
+
+    public function amount(): Attribute
+    {
+        return Attribute::make(
+            set: function(mixed $value) {
+                $this->attributes["amount"] = ToFloatParser::parse($value);
+            }
+        );
+    }
 }
