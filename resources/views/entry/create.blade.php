73b2f6f33f0fb09efebeec380fa1a213cdc554a5
BernardinoSlv <bernardinoosilvaa@gmail.com>
2023-12-09 07:49:36 -0300

    add view entry.create

---
+++ resources/views/entry/create.blade.php
@@ -0,0 +1,25 @@
+@extends('master.master')
+
+@section('content')
+    <form action="" method="POST">
+        @csrf
+        <div class="row gy-3 mb-4">
+            <div class="col-sm-8">
+                <label for="" class="form-label">Título</label>
+                <input type="text" name="title" class="form-control" value="{{ old('title') }}">
+                <div class="form-text">Ex: Pagamento do fulano</div>
+            </div>
+            <div class="col-sm-4">
+                <label for="" class="form-label">Valor</label>
+                <input type="text" name="amount" class="form-control" value="{{ old('amount') }}">
+            </div>
+            <div class="col-12">
+                <label for="" class="form-label">Descrição</label>
+                <textarea name="description" class="form-control" style="height: 140px"></textarea>
+            </div>
+        </div>
+        <div class="text-end">
+            <button type="submit" class="btn btn-primary">Criar</button>
+        </div>
+    </form>
+@endsection
