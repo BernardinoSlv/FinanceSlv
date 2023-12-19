76a4aedc8cbdd0e87e8c8bf35adad5703adf1100
BernardinoSlv <bernardinoosilvaa@gmail.com>
2023-12-19 19:28:56 -0300

    feat Entry model: added amount setter

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Src\Parsers\RealToFloatParser;
 use Src\Parsers\ToFloatParser;
 
 class Entry extends Model
@@ -16,4 +17,13 @@ class Entry extends Model
         "description",
         "amount",
     ];
+
+    public function amount(): Attribute
+    {
+        return Attribute::make(
+            set: function (mixed $value) {
+                $this->attributes["amount"] = RealToFloatParser::parse($value);
+            }
+        );
+    }
 }
