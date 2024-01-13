6ff5dfbbc9986ced955c59f4d9c9dedcf1aff7e1
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-01-13 10:00:58 -0300

    feat InvestimentController: index action

---
+++ app/Http/Controllers/InvestimentController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInvestimentRequest;
+use App\Http\Requests\UpdateInvestimentRequest;
+use App\Models\Investiment;
+use App\Repositories\Contracts\InvestimentRepositoryContract;
+
+class InvestimentController extends Controller
+{
+    public function __construct(
+        protected InvestimentRepositoryContract $_investimentRepository
+    ) {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $investiments = $this->_investimentRepository->allByUser(auth()->user()->id);
+
+        return view("investiment.index", compact(
+            "investiments"
+        ));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvestimentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Investiment $investiment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Investiment $investiment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvestimentRequest $request, Investiment $investiment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Investiment $investiment)
+    {
+        //
+    }
+}
---
+++ tests/Feature/Http/Controllers/InvestimentControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Investiment;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InvestimentControllerTest extends TestCase
+{
+    /**
+     * deve redirecionar para login
+     */
+    public function test_index_action_unauthenticated(): void
+    {
+        $this->get(route("investiments.index"))->assertRedirectToRoute("auth.index");
+    }
+
+    /**
+     * deve ter status 200 e view investiment.index
+     */
+    public function test_index_action(): void
+    {
+        Investiment::factory(20)->create();
+        $user = $this->_user();
+        Investiment::factory(10)->create([
+            "user_id" => $user->id
+        ]);
+
+        $this->actingAs($user)->get(route("investiments.index"))
+            ->assertOk()
+            ->assertViewIs("investiment.index")
+            ->assertViewHas("investiments", function (Collection $investiments): bool {
+                return $investiments->count() === 10;
+            });
+    }
+}
