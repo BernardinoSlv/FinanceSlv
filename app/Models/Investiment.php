0a5832134ce7ad6947295545394536389a9efded
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-01 10:04:45 -0300

    feat Investiment model: added leaves method

--- app/Models/Investiment.php
+++ app/Models/Investiment.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Investiment extends Model
@@ -17,4 +20,9 @@ class Investiment extends Model
         "amount",
         "description"
     ];
+
+    public function leaves(): MorphMany
+    {
+        return $this->morphMany(Leave::class, "leaveable");
+    }
 }
