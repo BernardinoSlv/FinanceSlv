5c8815f68b1cd79bfff02f0b6abe4229a7a3e65a
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-04 09:48:27 -0300

    feat QuickEntry model: add entry method

--- app/Models/QuickEntry.php
+++ app/Models/QuickEntry.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class QuickEntry extends Model
 {
@@ -15,4 +16,9 @@ class QuickEntry extends Model
         "description",
         "amount"
     ];
+
+    public function entry(): MorphOne
+    {
+        return $this->morphOne(Entry::class, "entryable");
+    }
 }
