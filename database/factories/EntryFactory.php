aff060886eafd30ee057ba97ae5973adabc95640
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-04 08:46:04 -0300

    refactor EntryFactory and LeaveFactory

--- database/factories/EntryFactory.php
+++ database/factories/EntryFactory.php
@@ -18,16 +18,8 @@ class EntryFactory extends Factory
      */
     public function definition(): array
     {
-        $user = User::factory()->create();
-        
         return [
-            "user_id" => $user,
-            "identifier_id" => Identifier::factory()->create([
-                "user_id" => $user
-            ]),
-            "title" => fake("pt_BR")->title(),
-            "description" => null,
-            "amount" => 10000
+            "user_id" => User::factory()->create(),
         ];
     }
 }
