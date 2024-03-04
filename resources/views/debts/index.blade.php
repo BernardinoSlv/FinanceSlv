8c86a05b24dd917529426767c60a186f41373981
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-04 08:21:06 -0300

    update view debts.index

--- resources/views/debts/index.blade.php
+++ resources/views/debts/index.blade.php
@@ -8,7 +8,7 @@
     {{-- <h3>{{ date('m/Y') }}</h3> --}}
 
     @include('includes.alerts')
-    <div class="table-responsive">
+    <div>
         <table class="table table-hover" style="min-width: 700px">
             <thead>
                 <tr>
@@ -17,7 +17,7 @@
                     <th>Valor</th>
                     <th>Status</th>
                     <th>Data</th>
-                    <th>Ações</th>
+                    <th>Ação</th>
                 </tr>
             </thead>
             <tbody>
@@ -28,25 +28,18 @@
                         <td>R$ {{ $debt->amount }}</td>
                         <td><span class="badge bg-success">Ativo</span></td>
                         <td>{{ $debt->created_at_formated }}</td>
-                        <td >
-                            <a class="btn btn-outline-primary btn-sm"
-                                href="{{ route('debts.edit', [
-                                    'debt' => $debt->id,
-                                ]) }}"><i
-                                    class="fas fa-edit"></i></a>
-                            <form
-                                action="{{ route('debts.destroy', [
-                                    'debt' => $debt->id,
-                                ]) }}"
-                                method="POST" class="d-inline-block">
-                                @csrf
-                                @method('DELETE')
-
-                                <button class="btn btn-danger btn-sm"
-                                    onclick="return confirm('Deseja remover a entrada #{{ $debt->id }}')">
-                                    <i class="fas fa-trash"></i>
+                        <td>
+                            <div class="dropdown">
+                                <button class="btn btn-sm btn-outline-secondary" data-bs-toggle="dropdown">
+                                    <i class="bi bi-three-dots"></i>
                                 </button>
-                            </form>
+
+                                <ul class="dropdown-menu">
+                                    <li><a href="#" class="dropdown-item">Editar</a></li>
+                                    <li><a href="#" class="dropdown-item">Pagamentos</a></li>
+                                    <li><a href="#" class="dropdown-item">Remover</a></li>
+                                </ul>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
