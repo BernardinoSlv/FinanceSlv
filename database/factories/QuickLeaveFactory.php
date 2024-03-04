879680a260122bebf4140fe79f5f8b3aa72677a0
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-04 08:53:27 -0300

    add QuickEntryFactory and QuickLeaveFactory

---
+++ database/factories/QuickLeaveFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Identifier;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\QuickLeave>
+ */
+class QuickLeaveFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::factory()->create();
+
+        return [
+            "user_id" => $user,
+            "identifier_id" => Identifier::factory()->create([
+                "user_id" => $user
+            ]),
+            "title" => fake("pt-BR")->title(),
+            "description" => fake("text"),
+            "amount" => 20.00,
+        ];
+    }
+}
