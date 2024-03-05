b720d2e53395b0ce738614569511a809f6934bd8
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-03-05 08:39:52 -0300

    feat QuickLeave model: leave method

--- app/Models/QuickLeave.php
+++ app/Models/QuickLeave.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class QuickLeave extends Model
@@ -17,4 +18,9 @@ class QuickLeave extends Model
         "description",
         "amount"
     ];
+
+    public function leave(): MorphOne
+    {
+        return $this->morphOne(Leave::class, "leaveable");
+    }
 }
