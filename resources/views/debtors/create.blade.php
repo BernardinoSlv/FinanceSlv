8528ba4f27c2082638f07b1518a9da04d34c12ab
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-04-29 10:28:34 -0300

    update view debtors/*

--- resources/views/debtors/create.blade.php
+++ resources/views/debtors/create.blade.php
@@ -1,34 +1,57 @@
 @extends('master.master')
 
 @section('content')
-    @include('includes.alerts')
+<!--breadcrumb-->
+<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+    <div class="breadcrumb-title pe-3">Operações</div>
+    <div class="ps-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0 p-0">
+                <li class="breadcrumb-item"><a href="{{ route('quick-entries.index') }}">Entradas rápidas</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Criar</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!--end breadcrumb-->
 
-    <form action="{{ route('debtors.store') }}" method="POST">
-        @csrf
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('debtors.store') }}" method="POST">
+                    @csrf
 
-        <div class="row gy-3 mb-4">
-            <div class="col-sm-6">
-                <label for="" class="form-label">Identificador</label>
-                <x-inputs.selects.identifier :identifiers="$identifiers" />
-            </div>
-            <div class="col-sm-6">
-                <label for="" class="form-label">Título</label>
-                <input type="text" name="title" class="form-control" value="{{ old('title') }}">
-                <div class="form-text"></div>
-            </div>
-            <div class="col-sm-6">
-                <label for="" class="form-label">Valor</label>
-                <input type="text" name="amount" class="form-control" value="{{ old('amount') }}">
-                <div class="form-text">Ex: 125,50</div>
-            </div>
-            <div></div>
-            <div class="col-12">
-                <label for="" class="form-label">Descrição</label>
-                <textarea name="description" class="form-control" style="height: 140px">{{ old('description') }}</textarea>
+                    @include("includes.alerts")
+
+                    <div class="row gy-3 mb-4">
+                        <div class="col-sm-6">
+                            <label for="" class="form-label">Identificador</label>
+                            <x-inputs.selects.identifier :identifiers="$identifiers" />
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="" class="form-label">Título</label>
+                            <input type="text" name="title" class="form-control" value="{{ old('title') }}">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="" class="form-label">Valor</label>
+                            <input type="text" name="amount" class="form-control" value="{{ old('amount') }}">
+                            <div class="form-text">Ex: 125,50</div>
+                        </div>
+                        <div></div>
+                        <div class="col-12">
+                            <label for="" class="form-label">Descrição</label>
+                            <textarea name="description" class="form-control" style="height: 140px">{{ old('description') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-primary">Criar</button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="text-end">
-            <button type="submit" class="btn btn-primary">Criar</button>
-        </div>
-    </form>
+    </div>
+
+</div><!--end row-->
 @endsection
--- resources/views/debtors/payments/edit.blade.php
+++ resources/views/debtors/payments/edit.blade.php
@@ -1,31 +1,56 @@
 @extends('master.master')
 
 @section('content')
-    @include('includes.alerts')
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Operações</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="">Entradas rápidas</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Editar #</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!--end breadcrumb-->
 
-    <form action="{{ route('debtors.payments.update', [
-        'debtor' => $debtor,
-        'entry' => $entry,
-    ]) }}"
-        method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <form
+                        action="{{ route('debtors.payments.update', [
+                            'debtor' => $debtor,
+                            'entry' => $entry,
+                        ]) }}"
+                        method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
 
-        <div class="row gy-3 mb-4">
+                        @include('includes.alerts')
 
-            <div class="col-sm-6">
-                <label for="" class="form-label">Comprovante</label>
-                <input type="file" name="images[]" multiple class="form-control" value="">
-                <div class="form-text"></div>
-            </div>
-            <div class="col-sm-6">
-                <label for="" class="form-label">Valor</label>
-                <input type="text" name="amount" class="form-control" value="{{ old('amount', $entry->amount) }}">
-                <div class="form-text">Ex: 125,50</div>
+                        <div class="row gy-3 mb-4">
+
+                            <div class="col-sm-6">
+                                <label for="" class="form-label">Comprovante</label>
+                                <input type="file" name="images[]" multiple class="form-control" value="">
+                                <div class="form-text"></div>
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="" class="form-label">Valor</label>
+                                <input type="text" name="amount" class="form-control"
+                                    value="{{ old('amount', $entry->amount) }}">
+                                <div class="form-text">Ex: 125,50</div>
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-primary">Atualizar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="text-end">
-            <button type="submit" class="btn btn-primary">Atualizar</button>
-        </div>
-    </form>
+
+    </div><!--end row-->
 @endsection
