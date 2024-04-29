40826a30d8027edd627d55d88371abca1e84b149
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-04-29 10:43:18 -0300

    update views investiments/*

--- resources/views/investiments/entries/create.blade.php
+++ resources/views/investiments/entries/create.blade.php
@@ -1,22 +1,43 @@
 @extends('master.master')
 
 @section('content')
-    @include('includes.alerts')
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Operações</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="{{ route('quick-entries.index') }}">Entradas rápidas</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Criar</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!--end breadcrumb-->
 
-    <form action="{{ route('investiments.entries.store', $investiment) }}" method="POST">
-        @csrf
-        <div class="row gy-3 mb-4">
-            <div class="col-sm-6">
-                <label for="" class="form-label">Comprovante</label>
-                <input type="file" name="" class="form-control">
-            </div>
-            <div class="col-6">
-                <label for="" class="form-label">Valor</label>
-                <input type="text" name="amount" value="{{ old('amount') }}" class="form-control">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('investiments.entries.store', $investiment) }}" method="POST">
+                        @csrf
+                        <div class="row gy-3 mb-4">
+                            <div class="col-sm-6">
+                                <label for="" class="form-label">Comprovante</label>
+                                <input type="file" name="" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label for="" class="form-label">Valor</label>
+                                <input type="text" name="amount" value="{{ old('amount') }}" class="form-control">
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-primary">Criar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="text-end">
-            <button type="submit" class="btn btn-primary">Criar</button>
-        </div>
-    </form>
+
+    </div><!--end row-->
 @endsection
--- resources/views/investiments/entries/edit.blade.php
+++ resources/views/investiments/entries/edit.blade.php
@@ -1,29 +1,51 @@
 @extends('master.master')
 
 @section('content')
-    @include('includes.alerts')
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Operações</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="">Entradas rápidas</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Editar #</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!--end breadcrumb-->
 
-    <form
-        action="{{ route('investiments.entries.update', [
-            'investiment' => $investiment,
-            'entry' => $entry,
-        ]) }}"
-        method="POST">
-        @method('PUT')
-        @csrf
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <form
+                        action="{{ route('investiments.entries.update', [
+                            'investiment' => $investiment,
+                            'entry' => $entry,
+                        ]) }}"
+                        method="POST">
+                        @method('PUT')
+                        @csrf
 
-        <div class="row gy-3 mb-4">
-            <div class="col-sm-6">
-                <label for="" class="form-label">Comprovante</label>
-                <input type="file" name="" class="form-control">
-            </div>
-            <div class="col-6">
-                <label for="" class="form-label">Valor</label>
-                <input type="text" name="amount" value="{{ old('amount', $entry->amount) }}" class="form-control">
+                        <div class="row gy-3 mb-4">
+                            <div class="col-sm-6">
+                                <label for="" class="form-label">Comprovante</label>
+                                <input type="file" name="" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label for="" class="form-label">Valor</label>
+                                <input type="text" name="amount" value="{{ old('amount', $entry->amount) }}"
+                                    class="form-control">
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-primary">Atualizar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="text-end">
-            <button type="submit" class="btn btn-primary">Atualizar</button>
-        </div>
-    </form>
+
+    </div><!--end row-->
 @endsection
