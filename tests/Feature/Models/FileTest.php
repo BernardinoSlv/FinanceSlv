502462da0a718c97416f36cbefb16737317b9960
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-05-23 18:46:17 -0300

    removed old tests

---
+++ tests/Feature/Models/FileTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * deve retornar o usuário mesmo 
+     *
+     * @return void
+     */
+    public function test_user_method(): void
+    {
+    }
+}
