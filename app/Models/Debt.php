1a470aeb290f7e01fd7e0536951ee3f9eb1db4b3
BernardinoSlv <bernardinoosilvaa@gmail.com>
2024-06-13 14:37:18 -0300

    feat Debt model: movements method

--- app/Models/Debt.php
+++ app/Models/Debt.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Debt extends Model
@@ -14,5 +15,10 @@ class Debt extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, "user_id", "id")->withTrashed();
+    }
+
+    public function movements(): MorphMany
+    {
+        return $this->morphMany(Movement::class, "movementable");
     }
 }
--- tests/Feature/Models/DebtTest.php
+++ tests/Feature/Models/DebtTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Debt;
+use App\Models\Movement;
+use App\Models\Quick;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -36,5 +38,37 @@ class DebtTest extends TestCase
         $debt = Debt::factory()->create(["user_id" => $user->id]);
 
         $this->assertEquals($user->id, $debt->user->id);
+    }
+
+    /**
+     * deve retornar uma coleção vazia
+     */
+    public function test_movements_method_without_movements(): void
+    {
+        Movement::factory(2)->for(Quick::factory(), "movementable")->create();
+
+        $debt = Debt::factory()->create();
+
+        $this->assertCount(0, $debt->movements);
+    }
+
+    /**
+     * deve retornar 2 Movement
+     */
+    public function test_movements_method(): void
+    {
+        Movement::factory(2)->for(Quick::factory(), "movementable")->create();
+
+        $debt = Debt::factory()->create();
+        Movement::factory(2)->create([
+            "movementable_type" => Debt::class,
+            "movementable_id" => $debt
+        ]);
+        Movement::factory(2)->trashed()->create([
+            "movementable_type" => Debt::class,
+            "movementable_id" => $debt
+        ]);
+
+        $this->assertCount(2, $debt->movements);
     }
 }
