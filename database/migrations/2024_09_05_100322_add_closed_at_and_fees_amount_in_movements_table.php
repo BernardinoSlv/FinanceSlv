99353fc81bc781ae3a8d1c80cd22a0a5537f46ba
bernardinoslv <bernardinoosilvaa@gmail.com>
2024-09-05 10:10:31 -0300

    add migration to add columns in movements table

---
+++ database/migrations/2024_09_05_100322_add_closed_at_and_fees_amount_in_movements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movements', function (Blueprint $table) {
+            $table->date("closed_at")
+                ->default(new Expression("(CURRENT_DATE)"))
+                ->after("effetive_at");
+            $table->decimal("fees_amount")->default(0)->after("closed_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movements', function (Blueprint $table) {
+            $table->dropColumn(["closed_at", "fees_amount"]);
+        });
+    }
+};
